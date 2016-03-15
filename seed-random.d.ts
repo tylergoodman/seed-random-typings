3e522771f0a3bf3d4598f31a046eb088dcdd5a48
tylergoodman <tylergoodman@users.noreply.github.com>
2016-03-14 19:26:12 -0700

    wrong format

--- seed-random.d.ts
+++ seed-random.d.ts
@@ -1,17 +1,16 @@
 
+declare function seed_random(seed?: any, options?: seed_random.options): seed_random.rand;
+declare namespace seed_random {
+  export function resetGlobal(): void;
 
-interface seed_random {
-  (seed?: any, options?: seed_random_options): rand;
-  resetGlobal(): void;
-}
+  interface rand {
+    (): number;
+  }
 
-interface rand {
-  (): number;
-}
-
-interface seed_random_options {
-  entropy?: boolean;
-  global?: boolean;
+  interface options {
+    entropy?: boolean;
+    global?: boolean;
+  }
 }
 
 export = seed_random;
